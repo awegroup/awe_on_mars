d153266d1cd14a717f39c62c6b623ddd07d40be1
r.schmehl@tudelft.nl <r.schmehl@tudelft.nl>
2023-10-24 11:43:34 +0200

    more

--- powercurve.py
+++ powercurve.py
@@ -3,7 +3,10 @@
 """
 Created on Mon Oct  9 22:37:27 2023
 
-Power curve computed following the three-regimes strategy
+Power curve computed following the three-regimes strategy.
+
+The accuracy of the wind speeds delimiting the three regimes is set by the
+wind_speed_delta parameter.
 
 @author: Roland Schmehl
 """
@@ -22,7 +25,7 @@ mpl.rcParams['pdf.fonttype'] = 42 # Output Type 3 (Type3) or Type 42 (TrueType)
 atmosphere_density        =  0.01    # kg/**3
 wind_speed_min            =  0.      # m/s
 wind_speed_max            =  50.     # m/s
-wind_speed_delta          =  1.      # m/s
+wind_speed_delta          =  1.      # m/s     ! accuracy of wind speed regimes
 
 # Kite properties
 kite_planform_area       =  200.    # m**2
@@ -55,10 +58,11 @@ power_factor_ideal = force_factor_out * cosine_beta_out**3 * 4/27
 
 wind_speed_range = wind_speed_max - wind_speed_min
 num_wind_speeds  = int(wind_speed_range/wind_speed_delta + 1)
-wind_speed       = np.linspace(wind_speed_min, wind_speed_max, num_wind_speeds)
-mechanical_power = []
-mechanical_power_ideal = []
+wind_speed  = np.linspace(wind_speed_min, wind_speed_max, num_wind_speeds)
+cycle_power = []
+power_ideal = []
 
+# Objective function for the three wind speed domains
 def objective_function_1(x):
     f_out    = x[0]
     f_in     = x[1]
@@ -75,6 +79,14 @@ def objective_function_2(x, mu_F, f_nF):
     return -(((cosine_beta_out - f_nF) / mu_F)**2  \
              - (gamma_in / force_factor_out) * a)  \
              * f_in*b/(mu_F*f_in-b)
+
+def objective_function_3(x, mu_P, f_nP):
+    f_in     = x[0]
+    a        = 1 - 2*f_in*cosine_beta_in + f_in**2
+    gamma_in = kite_lift_coefficient_in * np.sqrt(a/(1 - cosine_beta_in**2))
+    return -(((cosine_beta_out - f_nP) / mu_P)**2  \
+             - (gamma_in / force_factor_out) * a)  \
+             * f_in*f_nP/(mu_P*f_in-f_nP)
 
 print("num_wind_speeds = ", num_wind_speeds)
 
@@ -116,18 +128,21 @@ for v_w in wind_speed:
 
         # Reeling factors
         f_out = optimisation_result['x'][0]
-        f_in = optimisation_result['x'][1]
+        f_in  = optimisation_result['x'][1]
 
         # Normalized cycle power
         p_c = -objective_function_1 ([f_out, f_in])
 
+        # Tether force and mechanical power during reel out
         Ft_out = q * kite_planform_area * force_factor_out * \
                      (cosine_beta_out - f_out)**2
+        P_out  = Ft_out * v_w * f_out
 
         if Ft_out > nominal_tether_force:
             wind_speed_regime      = 2
             wind_speed_force_limit = v_w
             f_nF = f_out
+            print()
 
     # Constrained tether force
     if wind_speed_regime == 2:
@@ -144,20 +159,63 @@ for v_w in wind_speed:
                                           method='SLSQP')
 
         # Reeling factors
-        f_in = optimisation_result['x'][0]
+        f_out = (cosine_beta_out * (mu_F - 1) + f_nF)/mu_F
+        f_in  = optimisation_result['x'][0]
 
         # Normalized cycle power
         p_c = -objective_function_2 ([f_in], mu_F, f_nF)
 
+        # Tether force and mechanical power during reel out
+        Ft_out = q * kite_planform_area * force_factor_out * \
+                     (cosine_beta_out - f_out)**2
+        P_out  = Ft_out * v_w * f_out
+
+        if P_out > nominal_generator_power:
+            wind_speed_regime      = 3
+            wind_speed_power_limit = v_w
+            f_nP = f_out
+            print()
+
     # Constrained tether force and generator power
+    if wind_speed_regime == 3:
 
-    print(v_w, f_out, f_in, Ft_out, p_c)
+        mu_P = v_w / wind_speed_power_limit
 
-    mechanical_power.append(p_c * force_factor_out * kite_planform_area * Pw)
-    mechanical_power_ideal.append(power_factor_ideal * kite_planform_area * Pw)
+        starting_point = (-0.001)
+        bounds         = ((f_min, -0.001),)
 
-mechanical_power_min = np.min(mechanical_power_ideal)
-mechanical_power_max = np.max(mechanical_power_ideal)
+        optimisation_result = op.minimize(objective_function_3, \
+                                          starting_point,       \
+                                          args=(mu_F, f_nF),    \
+                                          bounds=bounds,        \
+                                          method='SLSQP')
+
+        # Reeling factors
+        f_out = f_nP / mu_P
+        f_in  = optimisation_result['x'][0]
+
+        # Normalized cycle power
+        p_c = -objective_function_3 ([f_in], mu_P, f_nP)
+
+        # Tether force and mechanical power during reel out
+        Ft_out = q * kite_planform_area * force_factor_out * \
+                     (cosine_beta_out - f_out)**2
+        P_out  = Ft_out * v_w * f_out
+
+    print("{:4.1f}".format(v_w),    \
+          "{:5.3f}".format(f_out),  \
+          "{:5.3f}".format(f_in),   \
+          "{:5.0f}".format(Ft_out), \
+          "{:6.0f}".format(P_out))
+
+#    print("K_a    =", "{:5.3f}".format(viking_1["speedofsound"]/ref),"   ", \
+#                      "{:5.3f}".format(arsia_north["speedofsound"]/ref))
+
+    cycle_power.append(p_c * force_factor_out * kite_planform_area * Pw)
+    power_ideal.append(power_factor_ideal * kite_planform_area * Pw)
+
+power_min = np.min(power_ideal)
+power_max = np.max(power_ideal)
 
 plt.figure()
 #plt.tight_layout()
@@ -167,6 +225,7 @@ plt.title('Power curve')
 plt.xlim([0, 40])
 plt.ylim([0, 40])
 plt.vlines(wind_speed_force_limit, 0, 40, colors='k', linestyles='solid')
-plt.plot(wind_speed, np.asarray(mechanical_power_ideal)/1000, 'r', linestyle=':', label=r"$f_{\mathrm{opt}}$")
-plt.plot(wind_speed, np.asarray(mechanical_power)/1000, 'b', linestyle='-', label=r"$f_{\mathrm{opt}}$")
+plt.vlines(wind_speed_power_limit, 0, 40, colors='r', linestyles='solid')
+plt.plot(wind_speed, np.asarray(power_ideal)/1000, 'r', linestyle=':', label=r"$f_{\mathrm{opt}}$")
+plt.plot(wind_speed, np.asarray(cycle_power)/1000, 'b', linestyle='-', label=r"$f_{\mathrm{opt}}$")
 plt.savefig("powercurve.svg")
